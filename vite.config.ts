e6f0a29aee9599dadb381ded4df5e96f6ff5c459
awallace689 <awallace689@gmail.com>
2024-11-18 01:41:49 -0600

    feat: attach black square canvas

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, "index.ts"),
       name: "canvas-render",
