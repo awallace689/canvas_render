dbc1a02ee62909ad754c77cb63020075193d00c5
awallace689 <awallace689@gmail.com>
2025-12-07 12:21:04 -0600

    fix tests

--- canvas-render/test/main.test.ts
+++ canvas-render/test/main.test.ts
@@ -1,9 +1,74 @@
-import { expect, describe, test } from 'vitest';
+import { expect, describe, test, beforeEach, afterEach, vi } from 'vitest';
 import { attach } from '../lib/main.ts';
+import { CANVAS_ID } from '../lib/constants.ts';
+
+const MOCK_CONTAINER_ID = 'mock-container-id';
 
 describe('attach', () => {
-    test('does a thing', () => {
-        const res = attach('some id');
-        expect(res).toEqual([1, 'asdf', 'some id']);
+    describe('with container', () => {
+        beforeEach(() => {
+            createContainer(MOCK_CONTAINER_ID);
+        });
+
+        afterEach(() => {
+            cleanupContainer(MOCK_CONTAINER_ID);
+        });
+
+        test('returns canvas ID after attaching', () => {
+            const res = attach(MOCK_CONTAINER_ID);
+
+            expect(res).toEqual(CANVAS_ID);
+        });
+
+        test('actually attaches canvas to container', () => {
+            attach(MOCK_CONTAINER_ID);
+            const container = document.querySelector(`#${MOCK_CONTAINER_ID}`);
+            const canvas = container!.querySelector(`canvas#${CANVAS_ID}`);
+
+            expect(canvas).not.toBeNull();
+        });
+
+        test('throws if canvas not attached to container', () => {
+            const container = document.querySelector(`#${MOCK_CONTAINER_ID}`);
+            container!.appendChild = vi.fn();
+
+            expect(() => attach(MOCK_CONTAINER_ID)).toThrow(
+                `Failed to attach element with id: ${MOCK_CONTAINER_ID}`
+            );
+            expect(container!.appendChild).toHaveBeenCalledTimes(1);
+        });
+
+        const createContainer = (containerId: string) => {
+            const container = document.createElement('div');
+            container.id = containerId;
+
+            document.body.appendChild(container);
+
+            return container;
+        };
+
+        const cleanupContainer = (containerId: string) => {
+            let container = document.querySelector(`#${containerId}`);
+            if (!container) {
+                throw new Error('Container not found.');
+            }
+
+            document.body.removeChild(container);
+
+            container = document.querySelector(`#${containerId}`);
+            if (container) {
+                throw new Error('Container not removed.');
+            }
+
+            return container;
+        };
+    });
+
+    describe('without container', () => {
+        test('cleans up', () => {
+            expect(() => attach(MOCK_CONTAINER_ID)).toThrow(
+                `Could not find element with id: ${MOCK_CONTAINER_ID}`
+            );
+        });
     });
 });
