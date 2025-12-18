811d8d25be86b051103ba71639f3d70284f6ba5d
awallace689 <awallace689@gmail.com>
2025-12-18 01:50:51 -0600

    fix err

--- canvas-render/lib/types.ts
+++ canvas-render/lib/types.ts
@@ -7,12 +7,8 @@ type Char = string & { __brand: 'character string' };
 export type CharColor = { char: Char; color: Color };
 
 export const isChar = (str: string): str is Char => str.length === 1;
-export const createCharColor = (
-    str: string,
-    color: Color,
-    assert = true
-): CharColor => {
-    if (!isChar(str) && assert) {
+export const createCharColor = (str: string, color: Color): CharColor => {
+    if (!isChar(str)) {
         throw new TypeAssertError('Char', str);
     }
 
